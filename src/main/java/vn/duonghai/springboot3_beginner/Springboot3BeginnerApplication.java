623c628b9299b7f038f36664328b7a925c05c3c9
DuongHai2202 <duongvanhai2202@gmail.com>
2025-11-04 09:52:52 +0700

    init project spring boot 3

---
+++ src/main/java/vn/duonghai/springboot3_beginner/Springboot3BeginnerApplication.java
@@ -0,0 +1,13 @@
+package vn.duonghai.springboot3_beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3BeginnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3BeginnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/duonghai/springboot3_beginner/Springboot3BeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.duonghai.springboot3_beginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3BeginnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
